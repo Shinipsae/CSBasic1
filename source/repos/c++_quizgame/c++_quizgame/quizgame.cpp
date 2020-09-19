bfbbfa78a4be8dbebfb82397600d4fc0dc0e2785
Shinipsae <s2019s13@e-mirim.hs.kr>
2020-09-19 23:20:43 +0900

    added main_screen()

--- source/repos/c++_quizgame/c++_quizgame/quizgame.cpp
+++ source/repos/c++_quizgame/c++_quizgame/quizgame.cpp
@@ -6,17 +6,19 @@
 
 void gotoxy(int x, int y) //커서 위치 조정 함수
 {
-	COORD Pos = { x - 1, y - 1 };
+	COORD Pos = { x-1, y-1 };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
 }
 
 void sc_design();
+void main_screen();
 
 int main() {
 	srand((unsigned)time(NULL)); //난수생성위한 srand() 함수
 	system("mode con cols = 120 lines = 30"); //콘솔 창 크기조절
 	sc_design(); //게임 창 디자인
 	gotoxy(60, 9); //좌표설정
+	main_screen();
 }
 
 void sc_design() //게임 디자인을 콘솔에 띄우는 함수
@@ -53,4 +55,21 @@ void sc_design() //게임 디자인을 콘솔에 띄우는 함수
 	printf("|                                                                                                                     |\n");
 	printf("|                                                                                                                     |\n");
 	printf("-----------------------------------------------------------------------------------------------------------------------");
+}
+
+void main_screen() {
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+	system("cls");
+	sc_design();
+
+	gotoxy(43, 7);
+	printf("C++ 초보 프로그래머를 위한 퀴즈 게임");
+	gotoxy(43, 11);
+	printf("1. 로그인");
+	gotoxy(43, 14);
+	printf("2. 회원가입");
+	gotoxy(43, 17);
+	printf("3. 도움말");
+	gotoxy(40, 21);
+	printf("원하시는 메뉴를 선택해주세요 >>");
 }
