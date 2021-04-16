a3392a71c65f3573aab4b971678adb05184f5b6d
Shinipsae <s2019s13@e-mirim.hs.kr>
2021-04-16 15:20:31 +0900

    first commit

---
+++ CSBasic1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSBasic1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "신" + "잎" + "새";
+            Console.Write(name);
+            Console.Write("개행이 안되는 녀석");
+            Console.WriteLine("개행이 되는 녀석");
+
+            var alpha = "알파";
+            // var 273alpha = "숫자이름";
+            var 한글변수명 = "된당";
+            // string 이모티콘 = "이모지는 안된다"
+                                                                                           
+        }
+    }
+}
