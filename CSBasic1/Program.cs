2e61da06acef0d9388de95291b24b5d1c1335a81
Shinipsae <s2019s13@e-mirim.hs.kr>
2021-04-19 16:07:34 +0900

    # 입력 Console.ReadLine()

--- CSBasic1/Program.cs
+++ CSBasic1/Program.cs
@@ -90,6 +90,21 @@ namespace CSBasic1
             var number5 = 100.0;
             var number6 = 100.0f;
 
+            string input = Console.ReadLine();
+            Console.WriteLine("input: " + input);
+            while (true)
+            {
+                input = Console.ReadLine();
+                if(input != "q")
+                {
+                    Console.WriteLine(input);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
 
         }
     }
